77c273669f61d89695732cfc2529e848f56a4857
Dineshchandar Ravichandran <dinesh.12693@gmail.com>
2022-10-07 19:55:52 -0400

    Assignment-3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 // use the express library
 const express = require('express');
 
+let nextVisitorId = 1;
+
 // create a new server application
 const app = express();
 
@@ -12,12 +14,101 @@ const port = process.env.PORT || 3000;
 
 
 // The main page of our website
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
+
+// True HTML
+// app.get('/', (req, res) => {
+//   res.send(`
+//     <!DOCTYPE html>
+//     <html lang="en">
+//       <head>
+//         <meta charset="UTF-8" />
+//         <title>An Example Title</title>
+//       </head>
+//       <body>
+//         <h1>Hello, World!</h1>
+//         <p>HTML is so much better than a plain string!</p>
+//       </body>
+//     </html>
+//   `);
+// });
+
+
+// The main page of our website with  dynamic html
+
+// //Direct injection
+
+// app.get('/', (req, res) => {
+//   // reads the url parameter
+//   // http://domain/?name=text
+//   const name = req.query.name || "World";
+
+//   res.send(`
+//     <!DOCTYPE html>
+//     <html lang="en">
+//       <head>
+//         <meta charset="UTF-8" />
+//         <title>An Example Title</title>
+//         <link rel="stylesheet" href="app.css">
+//       </head>
+//       <body>
+//         <h1>Hello, ${name}!</h1>
+//         <p>HTML is so much better than a plain string!</p>
+//       </body>
+//     </html>
+//   `);
+// });
+
+// //unsafe way
+// const {encode} = require('html-entities');
+
+
+// app.get('/', (req, res) => {
+//   // reads the url parameter
+//   // http://domain/?name=text
+//   const name = req.query.name || "World";
+
+//   res.send(`
+//     <!DOCTYPE html>
+//     <html lang="en">
+//       <head>
+//         <meta charset="UTF-8" />
+//         <title>An Example Title</title>
+//         <link rel="stylesheet" href="app.css">
+//       </head>
+//       <body>
+//         <h1>Hello, ${encode(name)}!</h1>
+//         <p>HTML is so much better than a plain string!</p>
+//       </body>
+//     </html>
+//   `);
+// });
+
+// //safer way
+// app.get('/', (req, res) => {
+//   res.render('welcome', {
+//     name: req.query.name || "World",
+//   });
+//   res.cookie('visited', Date.now().toString());
+// }); 
+
+// the main page with cookie
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.cookie('visitorId', nextVisitorId++);
+  res.cookie('visited', Date.now().toString());
+  res.render('welcome', /* params */)
 });
+
+//To allow sever to use public folder
+app.use(express.static('public'));
 
 // Start listening for network connections
 app.listen(port);
 
 // Printout for readability
 console.log("Server Started!");
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
